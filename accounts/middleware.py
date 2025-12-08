5d9ab4865e437be3c0d44e78bef4923454b099fc
tiva-OG <tivasalvation@gmail.com>
2025-12-08 19:52:00 +0100

    consumers <-> client-side seamless

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -3,7 +3,6 @@ from channels.db import database_sync_to_async
 from channels.middleware import BaseMiddleware
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.exceptions import TokenError
 
